cea971961ed9b7d1759affff43b4c3e3c0848cd1
Sofia <sofia.montgomery@gmail.com>
2026-02-27 19:32:31 +0100

    Move images, edit json, try json in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import lectureOne from "./data/lecture-one.json";
+import lectureTwo from "./data/lecture-two.json";
+import lectureThree from "./data/lecture-three.json";
 
 function App() {
   return (
     <>
       <h1>Klimatinfo</h1>
+      <h2>{lectureOne.title}</h2>
+      <p>{lectureOne.sections[0]}</p>
+      <h2>{lectureTwo.title}</h2>
+      <p>{lectureTwo.introduction}</p>
+      <h2>{lectureThree.title}</h2>
+      <img src={lectureThree.image.url} alt={lectureThree.image.alt} />
     </>
   );
 }
