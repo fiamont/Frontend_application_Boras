37b4f0f18658dd71c549adc51a119dcc71f307c6
Sofia <sofia.montgomery@gmail.com>
2026-02-27 19:54:21 +0100

    Move test-code to StartView instead

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import "./App.css";
-import lectureOne from "./data/lecture-one.json";
-import lectureTwo from "./data/lecture-two.json";
-import lectureThree from "./data/lecture-three.json";
-import mockUser from "./data/mock-user.json";
+import StartView from "./views/StartView";
 
 function App() {
   return (
     <>
       <h1>Klimatinfo</h1>
-      <h2>{lectureOne.title}</h2>
-      <p>{lectureOne.sections[0]}</p>
-      <h2>{lectureTwo.title}</h2>
-      <p>{lectureTwo.introduction}</p>
-      <h2>{lectureThree.title}</h2>
-      <img src={lectureThree.image.url} alt={lectureThree.image.alt} />
-      <h2>{`Välkommen tillbaka ${mockUser.username}!`}</h2>
+      <StartView />
     </>
   );
 }
--- src/views/StartView.jsx
+++ src/views/StartView.jsx
@@ -1,5 +1,21 @@
+import lectureOne from "../data/lecture-one.json";
+import lectureTwo from "../data/lecture-two.json";
+import lectureThree from "../data/lecture-three.json";
+import mockUser from "../data/mock-user.json";
+
 function StartView() {
-  return <div>Start</div>;
+  return (
+    <div>
+      <h2>Start</h2>
+      <h3>{lectureOne.title}</h3>
+      <p>{lectureOne.sections[0]}</p>
+      <h3>{lectureTwo.title}</h3>
+      <p>{lectureTwo.introduction}</p>
+      <h3>{lectureThree.title}</h3>
+      <img src={lectureThree.image.url} alt={lectureThree.image.alt} />
+      <h3>{`Välkommen tillbaka ${mockUser.username}!`}</h3>
+    </div>
+  );
 }
 
 export default StartView;
