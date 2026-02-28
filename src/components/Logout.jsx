1da24c88c32d9e0058185053d14cc102084f8c97
Sofia <sofia.montgomery@gmail.com>
2026-02-28 14:16:39 +0100

    Install and add react-icons to Logout

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,7 +1,11 @@
+import { IoIosArrowRoundForward } from "react-icons/io";
+
 function Logout() {
   return (
-    <div className="text-center fs-4">
-      <a href="#">Logga ut</a>
+    <div className="text-center fs-3">
+      <a href="#">
+        Logga ut <IoIosArrowRoundForward />
+      </a>
     </div>
   );
 }
