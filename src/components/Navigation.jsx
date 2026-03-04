326bb6ae9c09c208cbf69ccb68773ab2619471fe
Sofia <sofia.montgomery@gmail.com>
2026-03-04 21:09:03 +0100

    Change to React-Bootstrap components, add props and add separate css to Navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,55 @@
+import "./Navigation.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross1 } from "react-icons/rx";
 
 function Navigation(props) {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <nav
-      className={`${props.isLoginPage ? "navigation-login" : "navigation-default"} navigation p-2 d-flex gap-2 align-items-center`}
+    <Navbar
+      expanded={expanded}
+      onToggle={setExpanded}
+      expand="md"
+      className={`${props.isLoginPage ? "navigation-login" : "navigation-default"} p-2 d-flex gap-2 align-items-center`}
     >
-      <Link className="navigation-link" to="/start">
-        <h1>Klimatinfo</h1>
-      </Link>
-      <Link to="/start">Start</Link>
-      <Link to="/lektion">Utveckling</Link>
-      <Link to="/profil">{props.isLoginPage ? "Logga in" : "Profil"}</Link>
-    </nav>
+      <Container fluid>
+        <Link className="navigation-link" to="/start">
+          <h1>Klimatinfo</h1>
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          {expanded ? (
+            <RxCross1
+              className={`${props.isLoginPage ? "navigation-login" : "navigation-default"}`}
+            />
+          ) : (
+            <RxHamburgerMenu
+              className={`${props.isLoginPage ? "navigation-login" : "navigation-default"}`}
+            />
+          )}
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto gap-4">
+            <Link to="/start">Start</Link>
+            <NavDropdown title="Utveckling" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/lektion">
+                Koldioxidutsläpp
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/lektion">
+                Globala temperaturer
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/lektion">
+                Glaciärstorlekar
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Link to="/profil">
+              {props.isLoginPage ? "Logga in" : "Profil"}
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
