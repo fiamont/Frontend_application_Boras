48a433a541d95e29e24d37cb38301a3254756336
Sofia <sofia.montgomery@gmail.com>
2026-03-05 23:19:44 +0100

    Add some bootstrap and temporary custom css

--- src/components/InteractiveChart.jsx
+++ src/components/InteractiveChart.jsx
@@ -1,8 +1,11 @@
 function InteractiveChart() {
   return (
-    <div>
-      <div>Diagrammet</div>
-      <div>Select-knapparna</div>
+    <div className="d-flex flex-column align-items-center gap-2 pb-4">
+      <div className="d-flex flex-column p-3 chart-background container align-items-center">
+        <div className="chart-square"></div>
+        <h5 className="text-center pt-3">Beskrivningen på diagrammet</h5>
+      </div>
+      <div className="d-flex gap-3">Select-knapparna</div>
     </div>
   );
 }
