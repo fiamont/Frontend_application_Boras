b4ef8b22aa6b0082f079bcbd871d2664980bfec1
Sofia <sofia.montgomery@gmail.com>
2026-03-07 12:56:59 +0100

    Update LectureView with useParams so id routing works

--- src/views/LectureView.jsx
+++ src/views/LectureView.jsx
@@ -3,12 +3,26 @@ import lectureOne from "../data/lecture-one.json";
 import lectureTwo from "../data/lecture-two.json";
 import lectureThree from "../data/lecture-three.json";
 import Lecture from "../components/Lecture";
+import { useParams } from "react-router-dom";
 
 function LectureView() {
+  const params = useParams();
+  let lectureData = null;
+
+  if (params.id === "koldioxidutslapp") {
+    lectureData = lectureOne;
+  } else if (params.id === "globalatemperaturer") {
+    lectureData = lectureTwo;
+  } else if (params.id === "glaciarstorlekar") {
+    lectureData = lectureThree;
+  } else {
+    console.log("lektionen finns inte");
+  }
+
   return (
     <div>
       <Navigation isLoginPage={false} />
-      <Lecture lectureData={lectureOne} />
+      <Lecture lectureData={lectureData} />
     </div>
   );
 }
