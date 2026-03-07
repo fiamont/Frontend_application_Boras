317b6d77e898688383dd46ad5418a640179f81a3
Sofia <sofia.montgomery@gmail.com>
2026-03-07 15:13:23 +0100

    Install rechart package and add example chart from recharts website

--- src/components/InteractiveChart.jsx
+++ src/components/InteractiveChart.jsx
@@ -1,8 +1,11 @@
+import ResponsiveAreaChart from "./ResponsiveAreaChart";
+
 function InteractiveChart() {
   return (
     <div className="d-flex flex-column align-items-center gap-2 pb-4">
-      <div className="d-flex flex-column p-3 chart-background container align-items-center">
-        <div className="chart-square"></div>
+      <div className="d-flex flex-column container align-items-center">
+        {/* <div className="chart-square"></div> */}
+        <ResponsiveAreaChart />
         <h5 className="text-center pt-3">Beskrivningen på diagrammet</h5>
       </div>
       <div className="d-flex gap-3">Select-knapparna</div>
