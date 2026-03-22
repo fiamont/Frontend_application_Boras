4c14dba9b36345cbb3c56342ad5a3236b0285c4e
Sofia <sofia.montgomery@gmail.com>
2026-03-22 21:24:27 +0100

    Add Link to lecture image in profile view

--- src/components/UserLectures.jsx
+++ src/components/UserLectures.jsx
@@ -1,13 +1,16 @@
 import doneCarbon from "../assets/done-carbon-dioxide.png";
 import doneTemperature from "../assets/done-global-temperature.png";
 import beginGlacier from "../assets/begin-glacier.png";
+import { Link } from "react-router-dom";
 
 function UserLectures() {
   return (
     <div className="d-flex flex-column-reverse gap-3 flex-lg-row justify-content-between align-items-center">
       <img src={doneCarbon} alt="Bild på koldioxutsläpp" />
       <img src={doneTemperature} alt="Bild på globala temperaturer" />
-      <img src={beginGlacier} alt="Bild på glaciärstorlekar" />
+      <Link to="/lektion/glaciarstorlekar">
+        <img src={beginGlacier} alt="Bild på glaciärstorlekar" />
+      </Link>
     </div>
   );
 }
