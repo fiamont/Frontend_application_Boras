6a6a7687382f097bec01f5a25a5b13a9e7f1924d
Sofia <sofia.montgomery@gmail.com>
2026-04-05 22:34:46 +0200

    Update to dynamic value in progressbar

--- src/components/profile/ProfileProgress.jsx
+++ src/components/profile/ProfileProgress.jsx
@@ -1,12 +1,21 @@
 import "./Profile.css";
 import { ProgressBar } from "react-bootstrap";
 
-/*Visar användarens framsteg i lektioner med text och progressbar. Tar emot data som prop för att visa antal genomförda lektioner i texten.*/
+/*Visar användarens framsteg i lektioner med text och progressbar. Tar emot data som prop för att visa antal genomförda lektioner i texten och progressbar.*/
 function ProfileProgress({ data }) {
+  let progressValue = 0;
+
+  if (data.progress === "1") {
+    progressValue = 33;
+  } else if (data.progress === "2") {
+    progressValue = 66;
+  } else if (data.progress === "3") {
+    progressValue = 100;
+  }
   return (
     <div className="d-flex flex-column gap-3">
       <h2 className="text-center">{`${data.progress} av 3 lektioner genomförda!`}</h2>
-      <ProgressBar now={60} />
+      <ProgressBar now={progressValue} />
     </div>
   );
 }
